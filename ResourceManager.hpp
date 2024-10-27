4eb423dd5807e3c8a1e42ecbdf5f63451d4020c0
Krystian <zkrystian.z08@gmail.com>
2024-10-27 21:38:34 +0100

    Rozwiazane zadanie na 3/5

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,34 @@
 #pragma once
 
 #include "Resource.hpp"
+#include <iostream>
+#include <memory>
 
-class ResourceManager
-{
-    // Twoja implementacja tutaj
+class ResourceManager {
+private:
+    std::unique_ptr<Resource> resource;
+
+public:
+    ResourceManager() : resource(std::make_unique<Resource>()) {
+        std::cout << "ResourceManager created\n";
+    }
+
+    ~ResourceManager() {
+        std::cout << "ResourceManager destroyed\n";
+    }
+
+    ResourceManager(const ResourceManager& other) : resource(std::make_unique<Resource>(*other.resource)) {
+        std::cout << "ResourceManager copied\n";
+    }
+
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this != &other) {
+            resource = std::make_unique<Resource>(*other.resource);
+        }
+        std::cout << "ResourceManager copy-assigned\n";
+        return *this;
+    }
+    double get() const {
+        return resource->get();
+    }
 };
